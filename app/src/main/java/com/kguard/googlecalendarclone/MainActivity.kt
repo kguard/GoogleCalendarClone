51e56c5ccc6153a40804ef55bb4cced44dbf8488
kguard <kkh9331@naver.com>
2022-12-23 05:34:45 +0900

    toolbar 제작중

--- app/src/main/java/com/kguard/googlecalendarclone/MainActivity.kt
+++ app/src/main/java/com/kguard/googlecalendarclone/MainActivity.kt
@@ -2,10 +2,12 @@ package com.kguard.googlecalendarclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kguard.gitlogin.base.BaseActivity
+import com.kguard.googlecalendarclone.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
